0cfb2a00601d191ac51153777cf71b7800503025
Олехник Софья <aksyutik.sofiya@gmail.com>
2022-06-17 19:10:58 +0300

    t

--- my_app.py
+++ my_app.py
@@ -1,128 +1,55 @@
-from PyQt5.QtCore import QtCore
-from PyQt5.QtWidgets import (QApplication, QWidget, QHBoxLayout, QVBoxLayout, QGroupBox, QRadioButton, QPushButton, QLabel, QListWidget, QLineEdit)
-
-from instr import *
-from second_win import *
-
-class MainWin(QWidget):
-    def __init__(self):
-        super().__init__()
-        self.initUI()
-        self.connects()
-        self.set_appear()
-        self.show()
-    def initUI(self):
-        self.btn_next = QPushButton(txt_next, self)
-        self.hello_text = QLabel(txt_hello)
-        self.instruction = QLabel(txt_instruction)
-
-        self.layout_line = QVBoxLayout()
-        self.layout_line.addWidget(self.hello_text, alignment = Qt.AlignLeft)
-        self.layout_line.addWidget(self.instruction, alignment = Qt.AlignLeft)
-        self.layout_line.addWidget(self.btn_next, alignment = Qt.AlignCenter)
-
-        self.setLayout(self.layout_line)
-    def next_click(self):
-        self.tw = TestWin()
-        self.hide()
-
-    def connects(self):
-        self.btn_next.clicked.connect(self.next_click)
-
-    def set_appear(self):
-        self.setWindowTitle(txt_title)
-        self.resize(win_width, win_height)
-        self.move(win_x, win_y)
-
-app = QApplication([])
-mw = MainWin()
-app.axec_()
-
-
 from PyQt5.QtCore import Qt
 from PyQt5.QtWidgets import (
-QApplication, QWidget,
-QHBoxLayout, QVBoxLayout,
-QGroupbox, QRadioButton,
-QPushButton,QLabel, QListWidget, QLineEdit)
-
+       QApplication, QWidget,
+       QHBoxLayout, QVBoxLayout,
+       QGroupBox, QRadioButton,
+       QPushButton, QLabel, QListWidget, QLineEdit)
+ 
 from instr import *
-from final_win import *
-
-class TestWin(QWidget):
-    def __init__(self):
-        super().__init__()
-        self.initUI()
-        self.connects()
-        self.set_appear()
-        self.show()
-    def next_click(self):
-        self.tw = TestWin()
-        self.hide()
-    def connects(self):
-        self.btn_next.clicked.connect(self.next_click)
-    def set_appear(self):
-        self.setWindowtitle(txt_title)
-        self.resize(win_width, win_height)
-        self.move(win_x, win_y)
-    def initUI(self):
-        #self.questionnary = AllQuestions()
-        self.btn_next = QPushButton(txt_sendresults, self)
-        self.btn_test1  = QPushButton(txt_starttest1, self)
-        self.btn_test2  = QPushButton(txt_starttest2, self)
-        self.btn_test3  = QPushButton(txt_starttest3, self)
-
-
-        self.text_name = QLabel(txt_name)
-        self.text_age = QLabel(txt_age)
-        self.text_test1 = QLabel(txt_test1)
-        self.text_test2 = QLabel(txt_test2)
-        self.text_test3 = QLabel(txt_test3)
-        self.text_timer = QLabel(txt_timer)
-
-
-        self.line_name = QLineEdit(txt_hintname)
-
-        self.line_age = QLineEdit(txt_hintage)
-
-        self.line_test1  = QLineEdit(txt_hint_test1)
-
-        self.line_test2  = QLineEdit(txt_hint_test2)
-
-        self.line_test3  = QLineEdit(txt_hint_test3)
-
-
-        self.l_line  = QVBoxLayout()
-        self.r_line  = QVBoxLayout()
-        self.h_line  = QHBoxLayout()
-        self.r_line.addWidget(self.text_timer, alignment = Qt.AlignCenter)
-        self.l_line.addWidget(self.text_name, alignment = Qt.AlignLeft)
-        self.l_line.addWidget(self.line_name, alignment = Qt.AlignLeft)
-        self.l_line.addWidget(self.text_age, alignment = Qt.AlignLeft)
-        self.l_line.addWidget(self.line_age, alignment = Qt.AlignLeft)
-        self.l_line.addWidget(self.text_test1, alignment = Qt.AlignLeft)
-        self.l_line.addWidget(self.btn_test1, alignment = Qt.AlignLeft)
-        self.l_line.addWidget(self.line_test1, alignment = Qt.AlignLeft)
-        self.l_line.addWidget(self.text_test2, alignment = Qt.AlignLeft)
-        self.l_line.addWidget(self.btn_test2, alignment = Qt.AlignLeft)
-        self.l_line.addWidget(self.text_test3, alignment = Qt.AlignLeft)
-        self.l_line.addWidget(self.btn_test3, alignment = Qt.AlignLeft)
-        self.l_line.addWidget(self.line_test2, alignment = Qt.AlignLeft)
-        self.l_line.addWidget(self.line_test3, alignment = Qt.AlignLeft)
-        self.l_line.addWidget(self.btn_next, alignment = Qt.AlignCenter)
-        self.h_line.addLayout(self.l_line)
-        self.h_line.addLayout(self.r_line)
-        self.setLayout(self.h_line)
-    
-
-    def next_click(self):
-        self.hide()
-        self.fw = FinalWin()
-
-    def connects(self):
-        self.btn_next.clicked.connect(self.next_click)
-
-    def set_appear(self):
-        self.setWindowtitle(txt_title)
-        self.resize(win_width, win_height)
-        self.move(win_x, win_y)
+from second_win import *
+     
+class MainWin(QWidget):
+   def __init__(self):
+       ''' окно, в котором располагается приветствие '''
+       super().__init__()
+ 
+       # создаём и настраиваем графические элементы:
+       self.initUI()
+ 
+       #устанавливает связи между элементами
+       self.connects()
+ 
+       #устанавливает, как будет выглядеть окно (надпись, размер, место)
+       self.set_appear()
+ 
+       # старт:
+       self.show()
+ 
+   def initUI(self):
+       ''' создаёт графические элементы '''
+       self.btn_next = QPushButton(txt_next, self)
+       self.hello_text = QLabel(txt_hello)
+       self.instruction = QLabel(txt_instruction)
+ 
+       self.layout_line = QVBoxLayout()
+       self.layout_line.addWidget(self.hello_text, alignment = Qt.AlignLeft)
+       self.layout_line.addWidget(self.instruction, alignment = Qt.AlignLeft)
+       self.layout_line.addWidget(self.btn_next, alignment = Qt.AlignCenter)         
+       self.setLayout(self.layout_line)
+  
+   def next_click(self):
+       self.tw = TestWin()
+       self.hide()
+ 
+   def connects(self):
+       self.btn_next.clicked.connect(self.next_click)
+ 
+   ''' устанавливает, как будет выглядеть окно (надпись, размер, место) '''
+   def set_appear(self):
+       self.setWindowTitle(txt_title)
+       self.resize(win_width, win_height)
+       self.move(win_x, win_y)
+ 
+app = QApplication([])
+mw = MainWin()
+app.exec_()
